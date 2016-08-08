48391f0f9dbd0cb597c15a1e2e5ad1c4127420bf
Matyas Markovics <mm@issuu.com>
2016-08-08 10:22:21 +0200

    handle file upload and connect to database

--- Makefile
+++ Makefile
@@ -7,20 +7,22 @@ _venv:
 	virtualenv $@
 
 .PHONY: deps
-deps: _venv
+deps: _venv deps.txt 
 	source $</bin/activate; pip install -r $@.txt;
 
 .PHONY: run
 run: _venv deps db-test
-	source $</bin/activate; gunicorn -D main:app
+	source $</bin/activate; gunicorn --reload main:app &
 	sleep 1
 
 stop: db-stop
 	pkill -f gunicorn
 
 .PHONY: test
-test:
-	curl localhost:8000/things
+test: db-_test_data
+	curl -F "tsv=@mysql/_test/30b9cdb95aecb5981749/testdata.tsv" \
+			localhost:8000/things; \
+	curl localhost:8000/things;
 
 .PHONY: help
 help:
--- main.py
+++ main.py
@@ -1,26 +1,82 @@
-# things.py
-
-# Let's get this party started!
 import falcon
+from falcon_multipart.middleware import MultipartMiddleware
+import pymysql.cursors
+from contextlib import contextmanager
+import csv
+import tempfile
+import os
 
+@contextmanager
+def mysql_connector():
+    # Connect to the database
+    connection = pymysql.connect(unix_socket='/private/tmp/mysql.sock',
+                                 user='root',
+                                 #password='passwd',
+                                 host='localhost',
+                                 database='lastfm',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor,
+                                 autocommit=True,
+                                 local_infile=True)
+    try:
+        yield connection
+    finally:
+        connection.close()
+
+
+# falcon.API instances are callable WSGI apps
+app = falcon.API(middleware=[MultipartMiddleware()])
 
 # Falcon follows the REST architectural style, meaning (among
 # other things) that you think in terms of resources and state
 # transitions, which map to HTTP verbs.
 class ThingsResource(object):
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = ('\nTwo things awe me most, the starry sky '
-                     'above me and the moral law within me.\n'
-                     '\n'
-                     '    ~ Immanuel Kant\n\n')
 
-# falcon.API instances are callable WSGI apps
-app = falcon.API()
+    def on_post(self, req, resp, **kwargs):
+        tsv = req.get_param('tsv')
+        if tsv.file:
+            with tempfile.NamedTemporaryFile() as tmpf:
+                tmpf.write(tsv.file.read())
+                tmpf.flush()
+                with mysql_connector() as connection:
+                    with connection.cursor() as cursor:
+                        sql = """
+                        LOAD DATA LOCAL INFILE %s
+                        INTO TABLE log
+                        FIELDS TERMINATED BY '\t'
+                        (username, played_at, @dummy, artist, @dummy, track);
+                        """
+                        cursor.execute(sql, (tmpf.name))
+
+                        linecount = 0
+                        #tsv_reader = csv.reader(tsv.file, delimiter='\t')
+                        #for row in tsv_reader:
+                        #    # Create a new record
+                        #    sql = """
+                        #        INSERT IGNORE INTO log (username, artist, track, played_at)
+                        #        VALUES (
+                        #            %s, %s, %s,
+                        #            REPLACE(REPLACE(%s, 'T', ' '), 'Z', '')
+                        #        );
+                        #    """
+                        #    data = (row[0], row[3], row[5], row[1])
+                        #    #print cursor.mogrify(sql, data)
+                        #    cursor.execute(sql, data)
+                        #    linecount += 1
+                        resp.status = falcon.HTTP_200
+                        resp.body = "%s\n" % linecount
+
+
+    def on_get(self, req, resp):
+        with mysql_connector() as connection:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM user;"
+                cursor.execute(sql)
+                fmt = lambda l: '\t'.join(['%s'] * len(l)) % tuple(l)
+                resp.body = '\n'.join([fmt(r.values()) for r in cursor])
+                resp.status = falcon.HTTP_200
+
 
 # Resources are represented by long-lived class instances
-things = ThingsResource()
-
 # things will handle all requests to the '/things' URL path
-app.add_route('/things', things)
+app.add_route('/things', ThingsResource())
